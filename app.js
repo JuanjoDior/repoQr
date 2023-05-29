417a3c99842c0792d64c2a42c313e271028d5265
JuanjoDior <enjoyjdo94@gmail.com>
2023-05-29 22:16:41 +0200

    platilla 0.4

--- app.js
+++ app.js
@@ -1,45 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var video = document.getElementById("qr-video");
-    var canvasElement = document.getElementById("qr-canvas");
-    var canvas = canvasElement.getContext("2d");
-    var output = document.getElementById("output");
-  
-    var constraints = {
-      audio: false,
-      video: {
-        facingMode: "environment"
+  const video = document.getElementById("video");
+  const canvasElement = document.getElementById("canvas");
+  const canvas = canvasElement.getContext("2d");
+  const qrResult = document.getElementById("qr-result");
+
+  function handleSuccess(stream) {
+    video.srcObject = stream;
+    video.play();
+    requestAnimationFrame(tick);
+  }
+
+  function handleError(error) {
+    console.error("Error al acceder a la cámara: ", error);
+  }
+
+  function tick() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+      canvasElement.hidden = false;
+      canvasElement.width = video.videoWidth;
+      canvasElement.height = video.videoHeight;
+      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+      const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: "dontInvert"
+      });
+
+      if (code) {
+        qrResult.textContent = "Código QR detectado: " + code.data;
+        // Aquí puedes agregar la lógica adicional que desees realizar con el código QR detectado
+      } else {
+        qrResult.textContent = "Escaneando...";
       }
-    };
-  
-    function handleSuccess(stream) {
-      window.stream = stream;
-      video.srcObject = stream;
-      video.play();
-      requestAnimationFrame(tick);
     }
-  
-    function handleError(error) {
-      console.error("Error al acceder a la cámara: ", error);
-    }
-  
-    function tick() {
-      if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        canvasElement.height = video.videoHeight;
-        canvasElement.width = video.videoWidth;
-        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-        var code = jsQR(imageData.data, imageData.width, imageData.height, {
-          inversionAttempts: "dontInvert"
-        });
-  
-        if (code) {
-          output.innerHTML = "Código QR detectado: " + code.data;
-          // Aquí puedes agregar la lógica adicional que desees realizar con el código QR detectado
-        }
-      }
-      requestAnimationFrame(tick);
-    }
-  
-    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
-  });
-  
+    requestAnimationFrame(tick);
+  }
+
+  navigator.mediaDevices.getUserMedia({ video: true }).then(handleSuccess).catch(handleError);
+});
